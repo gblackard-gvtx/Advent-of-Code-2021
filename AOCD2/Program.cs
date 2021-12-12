2d4b37b981a25435c5f73da9c1cc0fa7edd0070d
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-12-12 00:27:26 -0600

    Add project files.

---
+++ AOCD2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+
+namespace AOC20221Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Users\Adam\source\repos\AOC2021\AOC20221Day2\day2.txt");
+
+            // part 1
+            /* int hor = 0;
+             int depth = 0;
+             for (int i = 0; i < lines.Length; i++)
+             {
+                 string[] dir = lines[i].Split(null);
+                 if (dir[0].Equals("forward"))
+                 {
+                     int move = Int32.Parse(dir[1]);
+                     hor += move;
+                 }
+                 else if (dir[0].Equals("down"))
+                 {
+                     int move = Int32.Parse(dir[1]);
+                     depth += move;
+                 }
+                 else if (dir[0].Equals("up"))
+                 {
+                     int move = Int32.Parse(dir[1]);
+                     depth -= move;
+                 }
+
+             }
+             Console.WriteLine(hor+"+"+depth+"="+(depth*hor));*/
+
+            //Part2
+            long hor = 0;
+            long depth = 0;
+            long aim = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] dir = lines[i].Split(null);
+                if (dir[0].Equals("forward"))
+                {
+                    int move = Int32.Parse(dir[1]);
+                    hor += move;
+                    Console.WriteLine(hor);
+                    depth = depth + ( move * aim);
+                    Console.WriteLine(depth);
+                }
+                else if (dir[0].Equals("down"))
+                {
+                    int move = Int32.Parse(dir[1]);
+                    aim += move;
+                    Console.WriteLine(aim);
+                }
+                else if (dir[0].Equals("up"))
+                {
+                    int move = Int32.Parse(dir[1]);
+                    aim -= move;
+                    Console.WriteLine(aim);
+                }
+
+            }
+            Console.WriteLine(depth*hor);
+
+            Thread.Sleep(300);
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Users\Adam\source\repos\AOC2021\ConsoleApp1\day1.txt");
+            int[] ints = Array.ConvertAll(lines, s => int.Parse(s));
+            //part 1
+            /* int first = ints[0];
+             int count = 0;
+             for (int i = 0; i < lines.Length; i++)
+             {
+                 if (first < ints[i])
+                 {
+                     count++;
+                     first = ints[i];
+                 }
+                 else { first = ints[i]; }
+             }
+             Console.WriteLine(count);*/
+            //part2
+            List<int> sums = new List<int>();
+            int count = 0;
+            int first = ints[0] + ints[1] + ints[2];
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if ((i+2)<ints.Length)
+                {
+                    sums.Add(ints[i] + ints[i + 1] + ints[i + 2]);
+                }
+                else
+                {
+                    break; 
+                }
+            }
+            foreach (var item in sums)
+            {
+                if (first<item)
+                {
+                    count++;
+                    first = item;
+                }
+                else { first = item; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
